e4919b6905b94bd814ed05a6377a2285bf8041d0
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-08-20 11:02:00 +0545

    gradients and autograds

---
+++ pytorch-essentials/autograd_and_gradients.py
@@ -0,0 +1,12 @@
+import torch 
+x= torch.tensor(2.0,requires_grad=True)
+y = x ** 2
+# print(y.backward())
+# print(x.grad)
+print(y)
+
+# A gradient tells us the direction and amount a value needs to change to affect the output.
+
+# require_grad = True --> this means "Keep track of operations involving x because later I want the gradient."
+
+# y.backward() --> "Calculate the gradient of y with respect to the variables that require gradients."
