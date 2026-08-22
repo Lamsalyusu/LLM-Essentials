7b9f190f12b157ed00920a2eaf65de6ca6e60582
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-08-22 11:06:34 +0545

    tokenization and embeddinggs using torch

--- pytorch-essentials/tokenization.py
+++ pytorch-essentials/tokenization.py
@@ -5,6 +5,23 @@ embeddings =nn.Embedding(
     embedding_dim = 4
 )
 
-tokens = torch.tensor([1,2,3,4])
+tokens = torch.tensor([1,2,3,4])  
+# this torch.tensor([1,2,3,4]) are token ids 0 -> I , 1 -> am like that
 output = embeddings(tokens)
 print(output,output.shape)
+
+# nnn_embeddings = 10: means there are 10 possible token IDs
+# 0,1,2,3,4,5,6,7,8,9 
+# i,am, a, very, good, person ,in ,my, life, bro.
+
+# embeddings_dim = 4 :  means each token gets a vector of 4 numbers 
+# conceptually:
+# Token 0 → [?, ?, ?, ?]
+# Token 1 → [?, ?, ?, ?]
+# Token 2 → [?, ?, ?, ?]
+# ...
+# Token 9 → [?, ?, ?, ?]
+
+# means each token gets a vector of 4 number 
+
+
