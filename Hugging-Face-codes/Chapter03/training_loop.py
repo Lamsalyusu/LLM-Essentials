e73f77e0074cab1b80ccc7a1fb4c0ab309cfc8b7
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-09-14 09:35:16 +0545

    pre-processing recap and tokenization function

--- Hugging-Face-codes/Chapter03/training_loop.py
+++ Hugging-Face-codes/Chapter03/training_loop.py
@@ -60,3 +60,41 @@
 # What exactly happens inside trainer.train()?
 
 # A manual loop gives you maximum control, but requires more code and more responsibility.
+
+# 2 Preprrocessing Recap
+from datasets import load_dataset
+from transformers import AutoTokenizer,DataCollatorWithPadding
+raw_datasets = load_datasets("nyu-mll/glue","mrpc")
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+# The original course may show load_dataset("glue", "mrpc"). In your environment, use nyu-mll/glue if the shorter identifier gives an error.
+
+# tokenizer function
+def tokenize_function(example):
+    return tokenizer(
+        example["sentence1"],
+        example["sentence2"],
+        truncation=True,
+    )
+
+apply it to every collaborator 
+tokenized_datasets = raw_datasets.map(
+    tokenize_function,
+    batched=True,
+)
+
+# dynamic padding collator
+data_collator = DataCollatorWithPadding(
+    tokenizer=tokenizer
+)
+
+# At this stage, each example contains fields such as:
+
+# sentence1
+# sentence2
+# label
+# idx
+# input_ids
+# token_type_ids
+# attention_mask
